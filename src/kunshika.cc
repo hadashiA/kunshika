1b392ac9b9b1e2e3d571ab2cabe8e08ec37f2cc8
f-kubotar <dev@hadashikick.jp>
2014-07-14 01:22:33 +0900

    remove config.status

--- src/kunshika.cc
+++ src/kunshika.cc
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv) {
     MeCab::Tagger *tagger = MeCab::createTagger("");
-    const MeCab::Node *first = tagger->parseToNode(argv[0]);
+    const MeCab::Node *first = tagger->parseToNode(argv[1]);
 
     for (const MeCab::Node *node = first->next; node->next; node = node->next) {
         std::string word(node->surface, node->length);
